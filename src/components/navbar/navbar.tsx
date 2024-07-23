3019f86bbb15be8db61575ab28bd9499171daca9
rezznr <reza.novian.r@gmail.com>
2024-07-23 23:26:00 +0700

    chore: Update navbar component to use nav element

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="px-[20vh] pt-10 absolute">
+    <nav className="px-[20vh] pt-10 absolute">
       <Link href="/">
         <Image width={150} src={logo} alt="nav-logo" />
       </Link>
-    </div>
+    </nav>
   );
 }
 
