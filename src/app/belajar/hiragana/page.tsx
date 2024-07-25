2e15b0cd24543dc0dd5e34490051700802ee91d1
rezznr <reza.novian.r@gmail.com>
2024-07-26 01:52:38 +0700

    feat: Add quiz.json for hiragana practice
    This commit adds the `quiz.json` file for hiragana practice. It includes a set of questions and answers in Japanese language. The file is located in the `public/quiz` directory.

--- src/app/belajar/hiragana/page.tsx
+++ src/app/belajar/hiragana/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1>Belajar</h1>
+      <h1>Hiragana</h1>
     </div>
   );
 };
---
+++ src/app/latihan/hiragana/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Quiz } from "./_partials/quiz";
+
+function Page() {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
+}
+
+export default Page;
