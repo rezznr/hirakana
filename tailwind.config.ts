4b6f26d4c3183dad69fd1142cc169ba9c9586a02
rezznr <reza.novian.r@gmail.com>
2024-08-02 19:17:08 +0700

    refactor: Update radial gradient styles and font family in tailwind.config.ts
    This commit updates the tailwind.config.ts file to include new radial gradient styles for background images. It modifies the existing "radial-gradient" options with different color combinations, improving the visual appearance of the application. Additionally, it introduces a new font family called "pottaOne" using the Potta One font and the "cursive" fallback. These changes enhance the design flexibility and overall visual appeal of the application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,19 +9,19 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "radial-gradient": "radial-gradient(circle, #FFDECF 0%, #FFEEEE 100%)",
+        "radial-gradient": "radial-gradient(circle, #FEFEFE 0%, #FCF2ED 100%)",
         "radial-gradient-100":
-          "radial-gradient(circle, #FFDECF 0%, #96FFA0 100%)",
+          "radial-gradient(circle, #FEFDFD 12%, #96FFA0 100%)",
         "radial-gradient-90":
-          "radial-gradient(circle, #FFDECF 0%, #C4FF96 100%)",
+          "radial-gradient(circle, #FEFDFD 12%, #C4FF96 100%)",
         "radial-gradient-80":
-          "radial-gradient(circle, #FFDECF 0%, #FFE896 100%)",
+          "radial-gradient(circle, #FEFDFD 12%, #FFE896 100%)",
         "radial-gradient-70":
-          "radial-gradient(circle, #FFDECF 0%, #FFC896 100%)",
+          "radial-gradient(circle, #FEFDFD 12%, #FFC896 100%)",
         "radial-gradient-60":
-          "radial-gradient(circle, #FFDECF 0%, #FF9696 100%)",
+          "radial-gradient(circle, #FEFDFD 12%, #FF9696 100%)",
         "radial-gradient-0":
-          "radial-gradient(circle, #E5E2E1 0%, #8B8B8B 100%)",
+          "radial-gradient(circle, #E5E2E1 12%, #8B8B8B 100%)",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
