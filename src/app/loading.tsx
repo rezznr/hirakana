6ba3a43e56ece9ed66a29b7cc7f8f403f07b37c3
rezznr <reza.novian.r@gmail.com>
2024-08-04 06:50:43 +0700

    refactor: Update animation and loading styles
    This commit updates the animation and loading styles in the code. It adds delay classes to the bouncing elements in the globals.css file, allowing for a staggered animation effect. Additionally, it modifies the Loading component in loading.tsx to include a customizable message prop and updates the styling of the loading indicator. These changes enhance the visual appeal and user experience of the application.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,22 @@
 export const runtime = "nodejs";
 
-export default function Loading() {
+export default function Loading({ message }: { message?: string }) {
   return (
-    <div className="flex items-center justify-center text-black mt-28">
-      <h2 className="text-2xl font-bold text-center md:text-4xl">Loading...</h2>
+    <div className="flex flex-col items-center justify-center gap-10 mt-28">
+      <div className="font-poppins flex flex-col items-center">
+        <h2 className="text-3xl text-[#251C1C] font-extrabold">
+          {message ? message : "Loading ..."}
+        </h2>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-row items-center justify-center gap-2">
+          <div className="w-12 h-12 bg-[#FF9696] rounded-full bouncing-element delay-0"></div>
+          <div className="w-12 h-12 bg-[#FF9696] rounded-full bouncing-element delay-1"></div>
+          <div className="w-12 h-12 bg-[#FF9696] rounded-full bouncing-element delay-2"></div>
+          <div className="w-12 h-12 bg-[#FF9696] rounded-full bouncing-element delay-3"></div>
+          <div className="w-12 h-12 bg-[#FF9696] rounded-full bouncing-element delay-4"></div>
+        </div>
+      </div>
     </div>
   );
 }
