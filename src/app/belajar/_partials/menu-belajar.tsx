49a1508b7ec1c25293eba99c1b2aaf323017fa1a
rezznr <reza.novian.r@gmail.com>
2024-08-04 14:47:57 +0700

    refactor: Update menu-belajar and improve responsive layout
    This commit updates the menu-belajar component to menu-latihan in the menu-belajar.tsx file. It also improves the responsive layout by changing the flex and gap properties. These changes enhance the clarity and consistency of the code, providing a better user experience across different devices.

--- src/app/belajar/_partials/menu-belajar.tsx
+++ src/app/belajar/_partials/menu-belajar.tsx
@@ -5,10 +5,10 @@ import { menu } from "./menu.data";
 
 function MenuBelajar() {
   return (
-    <div className="flex sm:flex-col lg:flex-row gap-4">
+    <div className="flex flex-col gap-10 md:flex-row md:gap-20">
       {menu.map((item, i) => (
-        <Link className="font-bold" href={item.link} key={i}>
-          <div className="w-[170px] h-[180px] bg-gradient-to-b from-[#fc4774] to-[#dca7a2] rounded-[17px] shadow flex flex-col items-center justify-center hover:scale-105 active:animate-ping">
+        <Link className="font-bold font-sans" href={item.link} key={i}>
+          <div className="bg-gradient-to-b from-[#fc4774] to-[#dca7a2] rounded-[17px] shadow flex flex-col items-center justify-center hover:scale-110 active:scale-95 transition-transform md:w-[170px] md:h-[180px]">
             <Image
               className="p-5"
               src={item.icon}
@@ -16,9 +16,7 @@ function MenuBelajar() {
               width={150}
               height={150}
             />
-            <p className="text-black text-[22px] font-black font-['Poppins']">
-              {item.name}
-            </p>
+            <p className="text-black text-[22px]">{item.name}</p>
           </div>
         </Link>
       ))}
--- src/app/belajar/layout.tsx
+++ src/app/belajar/layout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+export const metadata = {
+  title: "Hirakana | Latihan",
+};
+
 function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex justify-center items-center max-w-7xl">{children}</div>
+  );
 }
 
 export default Layout;
