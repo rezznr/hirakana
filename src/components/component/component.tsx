69cfbde410778f32156d9995a40c8027ad76c43e
rezznr <reza.novian.r@gmail.com>
2024-08-09 18:02:07 +0700

    refactor: Update component.tsx to optimize image loading and positioning
    Refactor the component.tsx file to optimize the loading and positioning of images. This includes updating the classNames and adding new images for better visual presentation. These changes improve the performance and user experience of the component.

--- src/components/component/component.tsx
+++ src/components/component/component.tsx
@@ -1,4 +1,4 @@
-import { gerbang, lamp3, orang1, orang2 } from "@assets/img";
+import { gerbang, lamp2, lamp3, orang1, orang2 } from "@assets/img";
 import Image from "next/image";
 import React from "react";
 
@@ -6,28 +6,34 @@ function Component() {
   return (
     <div>
       <Image
-        className="fixed left-[3%] bottom-0 hidden md:block"
+        className="fixed -z-20 left-[3%] bottom-0 hidden md:block"
         src={gerbang}
         width={300}
         height={300}
         alt=""
       />
       <Image
-        className="fixed left-[15%] bottom-0 hidden md:block"
+        className="fixed -z-10 left-[15%] bottom-0 hidden md:block"
         src={orang2}
         width={120}
         height={120}
         alt=""
       />
       <Image
-        className="fixed right-5 bottom-0 hidden md:block"
+        className="fixed -z-10 right-5 bottom-0 hidden md:block"
         src={orang1}
         width={120}
         alt=""
       />
       <Image
-        className="fixed right-5 top-0 w-[15%] block md:hidden"
+        className="absolute right-[5%] top-0 w-[9%] block md:hidden"
         src={lamp3}
+        width={50}
+        alt=""
+      />
+      <Image
+        className="absolute right-[15%] top-0 w-[10%] block md:hidden"
+        src={lamp2}
         width={50}
         alt=""
       />
