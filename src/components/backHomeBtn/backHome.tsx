c6bb27a8a1bd3f06dbbef17e2c42c00716ad11b2
rezznr <reza.novian.r@gmail.com>
2024-08-11 18:32:49 +0700

    refactor: Update BackHomeBtn component styling
    Improve the styling of the BackHomeBtn component by updating the CSS classes for better visual presentation. This enhances the user experience and navigation on the website.

--- src/components/backHomeBtn/backHome.tsx
+++ src/components/backHomeBtn/backHome.tsx
@@ -4,15 +4,15 @@ import { MdHome } from "react-icons/md";
 
 export function BackHomeBtn() {
   return (
-    <div className="fixed group bottom-[5%]">
+    <div className=" group">
       <Link
-        className="flex items-center bg-[#F94C76]/30 rounded-xl hover:bg-[#F94C76]/50 hover:scale-105 active:scale-100 transform transition duration-300 py-5 px-4 font-extrabold font-poppins"
+        className="flex items-center bg-[#F94C76]/50 rounded-xl hover:bg-[#F94C76]/90 hover:scale-105 active:scale-100 transform transition duration-300 py-5 px-4 font-extrabold font-poppins"
         href={"/"}
       >
         <MdHome className="inline-block text-xl md:text-2xl" />
       </Link>
       <div className="absolute bottom-auto mb-2 hidden group-hover:flex justify-center items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100">
-        <span className="bg-red-300 text-white text-sm rounded-xl py-1 px-2 shadow-lg whitespace-nowrap">
+        <span className="bg-[#F94C76]/50 text-white text-sm rounded-xl py-1 px-2 shadow-lg whitespace-nowrap">
           Kembali ke menu Awal
         </span>
       </div>
