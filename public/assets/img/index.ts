3d91c105c3e8ab1ec0a52cb801d235868c367e05
rezznr <reza.novian.r@gmail.com>
2024-08-22 19:19:10 +0700

    refactor: Remove unnecessary image imports and optimize image loading
    Remove unnecessary image imports and optimize image loading by removing unused images and implementing lazy loading for the remaining images. This improves the performance and reduces the bundle size of the application.

--- public/assets/img/index.ts
+++ public/assets/img/index.ts
@@ -1,14 +1,10 @@
-import kanjiRight from "./kanji-bottom-right.png";
 import gerbang from "./gerbang.png";
-import rantingKiri from "./ranting-kiri.png";
-import rantingKanan from "./ranting-sakura-kanan.png";
 import orang1 from "./orang-1.png";
 import orang2 from "./orang-2.png";
 import menuBelajar from "./menu-belajar.png";
 import menuLatihan from "./menu-latihan.png";
 import menuQuiz from "./menu-quiz.png";
 import logo from "./logo.png";
-import logoKanjiMid from "./kanji-mid.png";
 import lamp1 from "./lamp-1.png";
 import lamp2 from "./lamp-2.png";
 import lamp3 from "./lamp-3.png";
@@ -16,17 +12,13 @@ import hiragana from "./hiragana.png";
 import katakana from "./katakana.png";
 
 export {
-  kanjiRight,
   gerbang,
-  rantingKiri,
-  rantingKanan,
   orang1,
   orang2,
   menuBelajar,
   menuLatihan,
   menuQuiz,
   logo,
-  logoKanjiMid,
   lamp1,
   lamp2,
   lamp3,
